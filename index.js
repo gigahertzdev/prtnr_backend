ac9e4def16657ab8a70d5dbc82e331be8d8490fb
gigahertzdev <gigahertzdev@gmail.com>
2023-07-27 00:03:39 +0530

    edits

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.get(`/checkdeep`, async function (req, res) {
 		 headers: {
 		 	'Content-Type': 'application/json',
 		// 	'X-RapidAPI-Key': 'your-rapidapi-key'
-    'Accept': 'application/json, text/plain, */*',
+//    'Accept': 'application/json, text/plain, */*',
     
     body: JSON.stringify({
       "dynamicLinkInfo": {
@@ -87,7 +87,7 @@ app.get(`/checkdeep`, async function (req, res) {
 		.catch(err => console.error('error:' + err));*/
 	try {
 		let response = await fetch(url, options);
-		response = await response.text;
+		response = await response.json();
 		res.status(200).json({"data":response});
     return res;
 	} catch (err) {
