a2e604d13d69a2040672401983b7b4c46abc89c9
gigahertzdev <gigahertzdev@gmail.com>
2023-07-27 00:25:10 +0530

    edits

--- index.js
+++ index.js
@@ -93,8 +93,8 @@ app.get(`/checkdeep`, async function (req, res) {
 		.catch(err => console.error('error:' + err));*/
 	try {
 		let response = await fetch(url, options);
-		response = await response.json();
-		res.status(200).json({"daata":response});
+		response = await response.text();
+		res.status(200).json({"data":response});
     return res;
 	} catch (err) {
 		console.log(err);
