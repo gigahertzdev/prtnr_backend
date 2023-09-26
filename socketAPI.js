7925495437ca665f293f18ac25d7cef21251433c
gigahertzdev <gigahertzdev@gmail.com>
2023-09-27 06:16:17 +0900

    feat: setup socket for 3 choices

--- socketAPI.js
+++ socketAPI.js
@@ -3,21 +3,27 @@ import { Server } from "socket.io";
 export default function (server) {
 
     const io = new Server(server);
+    let mapEmail2socket = {};
 
     io.on('connection', (socket) => {
         console.log('A client connected');
  
-        socket.on('chat message', (message) => {
-          console.log('Received message:', message.data);
-          io.emit('chat message', message); // Broadcast the message to all connected clients
+        socket.on('mapEmail2socket', (data) => {
+          console.log('Received message:', data.email);
+          mapEmail2socket[data.email] = socket.id;
         });
  
         socket.on('disconnect', () => {
           console.log('A client disconnected');
         });
 
-        socket.on('determine3choices', (message) => {
+        socket.on('new 3choices', (data) => {
+          console.log(data)
+          io.to(mapEmail2socket[data.to]).emit('chat message', {}); // Broadcast the message to all connected clients
+        })
 
+        socket.on('sendOrder', (data) => {
+          io.to(mapEmail2socket[data.to]).emit('sendOrder', {});
         })
 
       });
