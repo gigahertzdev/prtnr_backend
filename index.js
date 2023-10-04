7a59da559af8bea54e00431e08af7c76b31cff8c
gigahertzdev <gigahertzdev@gmail.com>
2023-10-05 04:42:36 +0900

    migrate updates from bitbucket to github

--- index.js
+++ index.js
@@ -1,212 +1,154 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import nodemailer from 'nodemailer'
-import fetch from 'node-fetch';
-import http from "http";
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
+import twilio from "twilio";
+import axios from "axios";
 
-const app = express()
-const server = http.createServer(app);
-const port = 3030
+const app = express();
+const port = 3030;
 
 dotenv.config();
 
-import firestore from './src/firebase.js'
-
-import io from './socketAPI.js'
-io(server);
+import firestore from "./src/firebase.js";
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-// const route = express.Router();
+const route = express.Router();
+app.use("/", route);
 
+app.listen(port, () => {
+  //console.log(`Express backend is listening at http://localost:${port}....`)
+});
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: "gmail",
   auth: {
-    type: 'OAuth2',
+    type: "OAuth2",
     user: process.env.MAIL_USERNAME,
     pass: process.env.MAIL_PASSWORD,
     clientId: process.env.OAUTH_CLIENTID,
     clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN
-  }
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  },
 });
 
-app.get('/test', (req, res) => {
-  return res.send('Server is working..')
-})
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
 
-app.get('/deeplink', (req, res) => {
+route.post("/send-mail", (req, res) => {
+  const { to } = req.body;
 
-  /*
-  const url = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + api_key;
-
-  const xhttp = new XMLHttpRequest();
-
-    xhttp.open("GET", url, false);
-    xhttp.send();
-
-    const response = JSON.parse(xhttp.responseText);
-
-    console.log(response);*/
-
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-  return res.send('Testing.......')
-})
-
-
-
-
-/* For Testing Purpose ONLY */
-app.get(`/checkdeep`, async function (req, res) {
-
-    const url = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + api_key;
-  
-  const param={
-    "dynamicLinkInfo": {
-      "domainUriPrefix": "https://prtnr.page.link",
-      "link": "https://prtnr.page.link/V9Hh?email=sany@gmail.com",
-      
-    }
-  };
-    const options = {
-		method: 'POST',
-		 headers: {
-      'Content-Type': 'application/json',
-    
-   
-		 },
-     body: JSON.stringify(param)
-	};
-	try {
-		let response = await fetch(url, options);
-		response = await response.json();
-		res.status(200).json({"LSL":response,"res":response.shortLink});
-    return res;
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({msg: `Internal Server Error`});
-	}
-
-});
-
-
-app.post('/send-mail', (req, res) => {
-  const {to} = req.body;
+  console.log("send email", to);
   const verificationCode = Math.floor(Math.random() * 90000) + 10000;
-  const mailOptions = {
-    from: "gigahertzdev@gmail.com",
-    to,
-    subject: "Hi, Thanks for using Prtnr",
-    text: `Please confirm your verification code ${verificationCode} and type this on your app.`
-  };
   const userData = {
-    email: to,
+    phone: to,
     verifCode: verificationCode.toString(),
   };
-  console.log(userData);
+
   firestore.addVerificationData(userData, (result) => {
-    if(result == 'success') {
+    if (result == "success") {
       console.log("Registered on the database");
     } else {
-      console.log("error")
+      console.log("error");
     }
   });
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-      }
-      res.status(200).send({ message: "Mail send", message_id: info.messageId });
-  });
+  client.messages
+    .create({
+      body: `Please confirm your verification code ${verificationCode} and type this on your app.`,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: to,
+    })
+    .then((message) => {
+      console.log(message.sid);
+      res.status(200).send({ message: "Mail send", message_id: message.sid });
+    });
 });
 
+app.post("/sendInvitation", (req, res) => {
+  const { from, to } = req.body;
 
-
-app.post('/confirm-verification', (req, res) => {
-  const {email, verifCode} = req.body;
-  firestore.getRegisteredCode(email, (user) => {
-    if (user['data'] == 'true') {
-      if (verifCode == user['verification_code']) {
-        console.log("Your email address verified");
-        res.status(200).send({ message: "Your email address verified", title: "true"});
-      } else {
-        console.log("Verification Code is not valid.");
-        res.status(200).send({ message: "Verification Code is not valid.", title: "failed"})
-      }
-    }
-    else {
-      console.log("Your email is not verified, Please retry.");
-      res.status(200).send({ message: "Your email is not verified, Please retry.", title: "not exist"})
-    }
-  })
-  firestore.formatCollection(email, (result) => {
+  firestore.addInvitation(from, to, async (result) => {
     if (result == "success") {
-      console.log("Deleted");
-    } else {
-      console.log(result);
-    }
-  })
-});
-
-
-app.post('/sendInvitation', (req, res) => {
-  const {fromEmail, toEmail}  = req.body;
-
-  firestore.addInvitation(fromEmail, toEmail, async  (result) => {
-    if(result == 'success') {
       /* After Successfull data Addition send deep link also */
-     const url = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=' + process.env.WEB_API_KEY;
-        
-        const param={
-          "dynamicLinkInfo": {
-            "domainUriPrefix": "https://prtnr.page.link",
-            "link": "https://prtnr.page.link/V9Hh?email="+toEmail,
-            
-          }
-        };
-          const options = {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          
-        
-          },
-          body: JSON.stringify(param)
-        };
-        try {
-          let response = await fetch(url, options);
-          response = await response.json();
-          res.status(200).json({"email_to":toEmail,
-          "deep_link":response.shortLink,
-          success: true});
-          return res;
+      const url =
+        "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" +
+        process.env.WEB_API_KEY;
 
-        } catch (err) {
-          console.log(err);
-          res.status(500).json({msg: `Internal Server Error`,success: false});
-        }
+      const param = {
+        dynamicLinkInfo: {
+          domainUriPrefix: "https://prtnr.page.link",
+          link: "https://prtnr.page.link/V9Hh?email=" + to,
+        },
+      };
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(param),
+      };
+      try {
+        let response = await axios.post(url, param);
 
+        client.messages.create({
+          body: `Hey! Your partner sends you an invitation link: ${response.data.shortLink}`,
+          from: process.env.TWILIO_PHONE_NUMBER,
+          to: to,
+        });
 
-
-   //   console.log("Saved invitation successfully.");
- //     return res.send({success: true});
+        res.status(200).json({
+          toSms: to,
+          deep_link: response.data.shortLink,
+          success: true,
+        });
+        return res;
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: `Internal Server Error`, success: false });
+      }
     } else {
       console.log(result);
       return res.status(500).json({ error: result });
     }
   });
-})
+});
 
-// app.use('/', route);
-
-server.listen(port, () => {
-  console.log(`Express backend is listening at http://localost:${port}....`)
-})
-
+route.post("/confirm-verification", (req, res) => {
+  const { phone, verifCode } = req.body;
+  console.log(phone, verifCode);
+  firestore.getRegisteredCode(phone, (user) => {
+    console.log(user);
+    if (user["data"] == "true") {
+      if (verifCode == user["verification_code"]) {
+        console.log("Your phone number is verified");
+        res
+          .status(200)
+          .send({ message: "Your phone number is verified", title: "true" });
+      } else {
+        console.log("Verification Code is not valid.");
+        res.status(200).send({
+          message: "Verification Code is not valid.",
+          title: "failed",
+        });
+      }
+    } else {
+      console.log("Your phone number is not verified, Please retry.");
+      res.status(200).send({
+        message: "Your phone number is not verified, Please retry.",
+        title: "not exist",
+      });
+    }
+  });
+  firestore.formatCollection(phone, (result) => {
+    if (result == "success") {
+      console.log("Deleted");
+    } else {
+      console.log(result);
+    }
+  });
+});
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import {db} from './firebase-config.js';
+import { db } from "./firebase-config.js";
 import {
   doc,
   collection,
@@ -7,32 +7,30 @@ import {
   query,
   getDocs,
   deleteDoc,
-} from 'firebase/firestore';
-
+} from "firebase/firestore";
 
 const addVerificationData = async (userData, callback) => {
-  console.log(userData);
   try {
-    const docRef = await addDoc(collection(db, 'Verification'), {
-      email: userData.email,
+    const docRef = await addDoc(collection(db, "Verification"), {
+      phone: userData.phone,
       verification_code: userData.verifCode,
     });
     userData.userRefId = docRef.id;
-    callback('success');
+    callback("success");
   } catch (error) {
-    callback('error');
+    callback("error");
   }
 };
 
-const getRegisteredCode = async (emailId, callback) => {
-  let user = {data: 'false'};
+const getRegisteredCode = async (phone, callback) => {
+  let user = { data: "false" };
   try {
-    const userRef = collection(db, 'Verification');
-    const q = query(userRef, where('email', '==', emailId));
+    const userRef = collection(db, "Verification");
+    const q = query(userRef, where("phone", "==", phone));
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach(document => {
+    querySnapshot.forEach(async (document) => {
       user = document.data();
-      user['data'] = 'true';
+      user["data"] = "true";
     });
     callback(user);
   } catch (error) {
@@ -40,37 +38,36 @@ const getRegisteredCode = async (emailId, callback) => {
   }
 };
 
-const formatCollection = async (emailId, callback) => {
+const formatCollection = async (phone, callback) => {
   try {
-    const userRef = collection(db, 'Verification');
-    const q = query(userRef, where('email', '==', emailId));
+    const userRef = collection(db, "Verification");
+    const q = query(userRef, where("phone", "==", phone));
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach(async document => {
+    querySnapshot.forEach(async (document) => {
       console.log(document.id);
-      await deleteDoc(doc(db, 'Verification', document.id));
+      await deleteDoc(doc(db, "Verification", document.id));
     });
     callback("success");
   } catch (error) {
     callback(error);
   }
-}
+};
 
 const addInvitation = async (fromEmail, toEmail, callback) => {
   try {
-    const docRef = await addDoc(collection(db, 'Invitations'), {
-      fromEmail: fromEmail,
-      toEmail: toEmail,
+    const docRef = await addDoc(collection(db, "Invitations"), {
+      fromNumber: fromEmail,
+      toNumber: toEmail,
     });
-    callback('success');
+    callback("success");
   } catch (error) {
     callback(error);
   }
-}
-
+};
 
 export default {
   addVerificationData,
   getRegisteredCode,
   formatCollection,
-  addInvitation
+  addInvitation,
 };
